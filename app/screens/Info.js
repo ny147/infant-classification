59ea4f283b4abc8a04ba24e58f1fd1d49350ce7a
ronsiralak <34647983+ronsiralak@users.noreply.github.com>
2023-03-03 16:13:11 +0700

    Add BG in Info.js

--- app/screens/Info.js
+++ app/screens/Info.js
@@ -4,10 +4,12 @@ import { LinearGradient } from 'expo-linear-gradient';
 
 const Info = () =>{
     return (
-        <LinearGradient
-        colors={['#9DCEFF', '#92A3FD']}
+        <View
         style={styles.container}>
 
+            <Image source={{uri: 'https://cdn.discordapp.com/attachments/1080379783323582464/1081138438247563304/BlueCurve.png'}}  
+            style={{width: 412,height: 230,position: 'absolute',top:0,left:0}} />
+           
             <Text style={styles.Topic} >
                 Dustan Baby Language
             </Text>
@@ -20,102 +22,102 @@ const Info = () =>{
             </Text>
 
             <Image source={{uri: 'https://cdn.discordapp.com/attachments/1080379783323582464/1081099578490753024/Pink.png'}}  
-            style={{width: 60,height: 60,position: 'absolute',top:230,left:10,borderRadius: 65,}} />
-            <Text style={{top:243,left:20,color: 'white',fontSize:20,fontWeight: 'bold',position: 'absolute'}}>
+            style={{width: 60,height: 60,position: 'absolute',top:295,left:10,borderRadius: 65,}} />
+            <Text style={{top:308,left:20,color: 'white',fontSize:18,fontWeight: 'bold',position: 'absolute'}}>
                 Neh
             </Text>
 
-            <Text style={{top:240,left:75,color: 'black',fontSize:20,fontWeight: 'bold'}}>
+            <Text style={{top:300,left:75,color: 'black',fontSize:16,fontWeight: 'bold'}}>
                 Hungry
             </Text>
 
-            <Text style={{top:240,left:75,color: 'black',fontSize:14,}}>
+            <Text style={{top:300,left:75,color: 'black',fontSize:12,}}>
             Baby feel hungry. feeding breast milk or formula to  
             </Text>
-            <Text style={{top:240,left:75,color: 'black',fontSize:14,}}>
+            <Text style={{top:300,left:75,color: 'black',fontSize:12,}}>
             him/her
             </Text>
 
 
             <Image source={{uri: 'https://cdn.discordapp.com/attachments/1080379783323582464/1081099578490753024/Pink.png'}}  
-            style={{width: 60,height: 60,position: 'absolute',top:310,left:10,borderRadius: 65,}} />
-            <Text style={{top:325,left:20,color: 'white',fontSize:20,fontWeight: 'bold',position: 'absolute'}}>
+            style={{width: 60,height: 60,position: 'absolute',top:360,left:10,borderRadius: 65,}} />
+            <Text style={{top:375,left:20,color: 'white',fontSize:18,fontWeight: 'bold',position: 'absolute'}}>
                 Earih
             </Text>
 
-            <Text style={{top:240,left:75,color: 'black',fontSize:20,fontWeight: 'bold'}}>
+            <Text style={{top:300,left:75,color: 'black',fontSize:16,fontWeight: 'bold'}}>
                 Poop
             </Text>
 
-            <Text style={{top:240,left:75,color: 'black',fontSize:14,}}>
+            <Text style={{top:300,left:75,color: 'black',fontSize:12,}}>
             Baby  want to poop. stimulate bowel movements with
             </Text>
-            <Text style={{top:240,left:75,color: 'black',fontSize:14,}}>
+            <Text style={{top:300,left:75,color: 'black',fontSize:12,}}>
             gentle massage or warm water, and change their
             </Text>
-            <Text style={{top:240,left:75,color: 'black',fontSize:14,}}>
+            <Text style={{top:300,left:75,color: 'black',fontSize:12,}}>
             diaper promptly.
             </Text>
 
 
             <Image source={{uri: 'https://cdn.discordapp.com/attachments/1080379783323582464/1081099578490753024/Pink.png'}}  
-            style={{width: 60,height: 60,position: 'absolute',top:380,left:10,borderRadius: 65,}} />
-            <Text style={{top:395,left:20,color: 'white',fontSize:20,fontWeight: 'bold',position: 'absolute'}}>
+            style={{width: 60,height: 60,position: 'absolute',top:430,left:10,borderRadius: 65,}} />
+            <Text style={{top:445,left:20,color: 'white',fontSize:18,fontWeight: 'bold',position: 'absolute'}}>
                 Owh
             </Text>
 
-            <Text style={{top:240,left:75,color: 'black',fontSize:20,fontWeight: 'bold'}}>
+            <Text style={{top:305,left:75,color: 'black',fontSize:16,fontWeight: 'bold'}}>
                 Tried
             </Text>
 
-            <Text style={{top:240,left:75,color: 'black',fontSize:14,}}>
+            <Text style={{top:305,left:75,color: 'black',fontSize:12,}}>
             Baby feel tired. recommended to place the baby in   
             </Text>
-            <Text style={{top:240,left:75,color: 'black',fontSize:14,}}>
+            <Text style={{top:305,left:75,color: 'black',fontSize:12,}}>
             a safe sleeping position on their back
             </Text>
 
 
             <Image source={{uri: 'https://cdn.discordapp.com/attachments/1080379783323582464/1081099578490753024/Pink.png'}}  
-            style={{width: 60,height: 60,position: 'absolute',top:445,left:10,borderRadius: 65,}} />
-            <Text style={{top:460,left:20,color: 'white',fontSize:20,fontWeight: 'bold',position: 'absolute'}}>
+            style={{width: 60,height: 60,position: 'absolute',top:495,left:10,borderRadius: 65,}} />
+            <Text style={{top:510,left:20,color: 'white',fontSize:18,fontWeight: 'bold',position: 'absolute'}}>
                 Heh
             </Text>
 
-            <Text style={{top:240,left:75,color: 'black',fontSize:20,fontWeight: 'bold'}}>
+            <Text style={{top:320,left:75,color: 'black',fontSize:16,fontWeight: 'bold'}}>
                 Discomfort
             </Text>
 
-            <Text style={{top:240,left:75,color: 'black',fontSize:14,}}>
+            <Text style={{top:320,left:75,color: 'black',fontSize:12,}}>
             Baby feel discomfort. Check him/her diaper  
             </Text>
-            <Text style={{top:240,left:75,color: 'black',fontSize:14,}}>
+            <Text style={{top:320,left:75,color: 'black',fontSize:12,}}>
             and change it. 
             </Text>
 
 
             <Image source={{uri: 'https://cdn.discordapp.com/attachments/1080379783323582464/1081099578490753024/Pink.png'}}  
-            style={{width: 60,height: 60,position: 'absolute',top:510,left:10,borderRadius: 65,}} />
-            <Text style={{top:525,left:28,color: 'white',fontSize:20,fontWeight: 'bold',position: 'absolute'}}>
+            style={{width: 60,height: 60,position: 'absolute',top:560,left:10,borderRadius: 65,}} />
+            <Text style={{top:575,left:28,color: 'white',fontSize:18,fontWeight: 'bold',position: 'absolute'}}>
                 Eh
             </Text>
 
-            <Text style={{top:240,left:75,color: 'black',fontSize:20,fontWeight: 'bold'}}>
+            <Text style={{top:325,left:75,color: 'black',fontSize:16,fontWeight: 'bold'}}>
                 Burping
             </Text>
 
-            <Text style={{top:240,left:75,color: 'black',fontSize:14,}}>
+            <Text style={{top:325,left:75,color: 'black',fontSize:12,}}>
             Baby want to Burp. hold them upright and gently pat   
             </Text>
-            <Text style={{top:240,left:75,color: 'black',fontSize:14,}}>
+            <Text style={{top:325,left:75,color: 'black',fontSize:12,}}>
             or rub their back until they burp or show
             </Text>
-            <Text style={{top:240,left:75,color: 'black',fontSize:14,}}>
+            <Text style={{top:325,left:75,color: 'black',fontSize:12,}}>
             signs of relief.
             </Text>
 
         
-        </LinearGradient>
+        </View>
     )
 }
 
@@ -123,30 +125,24 @@ const Info = () =>{
 const styles = StyleSheet.create({
     container: {
       flex: 1,
+      backgroundColor: 'white',
     },
     Topic: {
         color: 'black',
-        fontSize : 36,
+        fontSize : 22,
         fontWeight: 'bold',
         position: 'absolute',
         margin:10,
-        top: 100,
-        left:11,  
+        top: 215,
+        left:70,  
       },
       Topic2: {
         color: 'black',
-        fontSize : 16,
-        //fontWeight: 'bold',
+        fontSize : 14,
         position: 'absolute',
         margin:10,
-        top: 150,
+        top: 245,
         left:0,  
-      },
-      TextTopic: {
-        color: 'red',
-        fontSize:24,
-        fontWeight: 'bold',
-        
       },
 
   });
