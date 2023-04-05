5afbcef37ee57986b313820b364bf479cfccd000
ronsiralak <34647983+ronsiralak@users.noreply.github.com>
2023-04-05 15:11:54 +0700

    update & clear dump file

---
+++ app/component/Graph.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import { View } from 'react-native'
+
+class Graph extends React.PureComponent {
+    
+    
+    render() {
+        const DA = this.props.data;
+        const data = DA
+        // const data = [
+        //     {
+        //         value: 50,
+        //         label: 'One',
+        //     },
+        //     {
+        //         value: 10,
+        //         label: 'Two',
+        //     },
+        //     {
+        //         value: 40,
+        //         label: 'Three',
+        //     },
+        //     {
+        //         value: 95,
+        //         label: 'Four',
+        //     },
+        //     {
+        //         value: 85,
+        //         label: 'Five',
+        //     },
+        // ]
+        //     console.log('graph',DA);
+            // console.log(data)
+
+        return (
+            <View>
+            </View>
+        )
+    }
+}
+
+
+export default Graph
